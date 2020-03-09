71b08b80ca23ba3de2e7d4e0567bab4e01e54aed
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-03-09 05:04:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const cards = [];
+    for(let i = 0; i < array.length; i++){
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    };
+    return cards;
+};
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    };
+};
