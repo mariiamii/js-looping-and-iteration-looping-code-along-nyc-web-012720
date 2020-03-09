eb3b637860138fcddfd5572bfde0c357a4238433
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-03-09 05:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 function writeCards(array, event) {
-    const cards = [];
+    const newArray = [];
     for(let i = 0; i < array.length; i++){
         cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
     };
-    return cards;
+    return newArray;
 };
 
 function countDown(number) {
