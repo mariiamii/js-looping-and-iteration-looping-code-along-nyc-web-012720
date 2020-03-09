0b660fdb8f175fd485a1846daa822f1f9a46d845
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-03-09 05:00:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function writecards(array, event) {
+    let newArray = [];
+    for (i=0; i < array.length;i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    };
+    return newArray;
+};
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    };
+};
